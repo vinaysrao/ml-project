f0f3485a42cd4b2d7c923f6390a184840c176f45
Vinay Rao <sr.vinay@gmail.com>
2012-11-10 19:20:49 +0530

    Added a simple quadratic curve fitting algorithm

---
+++ curve_fit.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python
+
+from numpy import linalg
+from numpy import matrix
+
+
+def xWithY( xdata, ydata ):
+	xwithy = []
+	l = len( xdata )
+	for i in xrange( l ):
+		xwithy.append( xdata[ i ] * ydata[ i ] )
+	return xwithy
+
+
+def fitQuadratic( xdata, ydata ):
+	'''
+	Uses method of least squares to find a, b, c to 
+	fit into a quadratic of the form a.x^2 + b.x + c
+	'''
+
+	if len( xdata ) != len( ydata ):
+		return
+	yi = sum( ydata )
+	xiyi = sum( xWithY( xdata, ydata ) )
+	xi2yi = sum( xWithY( [ i ** 2 for i in xdata ], ydata ) )
+
+	x = []
+	for i in xrange( 1, 5 ):
+		x.append( sum( [ j ** i for j in xdata ] ) )
+
+	A = matrix( [ [ len( xdata ), x[ 0 ], x[ 1 ] ], x[ 0 : 3 ], x[ 1 : ] ] )
+	X = [ yi, xiyi, xi2yi ]
+
+	a, b, c = linalg.solve( A, X )
+	return ( c, b, a )
+
+
+def yForX( x, abc ):
+	a, b, c = abc
+	return a * ( x ** 2 ) + b * x + c
+
+if __name__ == '__main__':
+	import matplotlib.pyplot as plt
+	from numpy import arange
+
+	xdata = [ 1, 2, 3, 4, 5 ]
+	ydata = [ 1, 4, 8, 17, 25 ]
+
+	abc = fitQuadratic( xdata, ydata )
+	print abc
+
+	X = arange( min( xdata ), max( xdata ) + 1 )
+	Y = [ yForX( x, abc ) for x in X ]
+	plt.plot( xdata, ydata, 'o', X, Y, 'k' )
+
+	plt.show()
