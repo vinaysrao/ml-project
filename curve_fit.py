ae4133e0b1c0c72e0f439ac1cee3e61ffa6aa4ce
Vinay Rao <sr.vinay@gmail.com>
2012-11-10 20:05:14 +0530

    Added linear fit

--- curve_fit.py
+++ curve_fit.py
@@ -35,22 +35,48 @@ def fitQuadratic( xdata, ydata ):
 	return ( c, b, a )
 
 
-def yForX( x, abc ):
-	a, b, c = abc
-	return a * ( x ** 2 ) + b * x + c
+def fitLinear( xdata, ydata ):
+	'''
+	Finds a, b to fit into a line of form a.x + b
+	'''
+	if len( xdata ) != len( ydata ):
+		return
+	yi = sum( ydata )
+	xiyi = sum( xWithY( xdata, ydata ) )
+	x = []
+	for i in xrange( 1, 3 ):
+		x.append( sum( [ j ** i for j in xdata ] )  )
 
-if __name__ == '__main__':
+	A = matrix( [ [ len( xdata ), x[ 0 ] ], x ] )
+	X = [ yi, xiyi ]
+
+	a, b = linalg.solve( A, X )
+	return ( b, a )
+
+
+def yForX( x, abc ):
+	try:
+		a, b, c = abc
+		return a * ( x ** 2 ) + b * x + c
+	except:
+		a, b = abc
+		return a * x + b
+
+if __name__ == '__main__': #Then show this example
 	import matplotlib.pyplot as plt
 	from numpy import arange
 
-	xdata = [ 1, 2, 3, 4, 5 ]
-	ydata = [ 1, 4, 8, 17, 25 ]
+	xdata = [ 1, 2, 3, 4, 5, 6, 7 ]
+	ydata = [ 1, 4, 8, 17, 25, 31, 50 ]
 
 	abc = fitQuadratic( xdata, ydata )
 	print abc
 
 	X = arange( min( xdata ), max( xdata ) + 1 )
 	Y = [ yForX( x, abc ) for x in X ]
-	plt.plot( xdata, ydata, 'o', X, Y, 'k' )
+
+	ab = fitLinear( xdata, ydata )
+	Y2 = [ yForX( x, ab ) for x in X ]
+	plt.plot( xdata, ydata, 'o', X, Y, 'k', X, Y2 )
 
 	plt.show()
