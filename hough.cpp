0a55c812b44bc26d2232dec9b06317e2c7522a2c
Vinay Rao <sr.vinay@gmail.com>
2012-11-17 11:15:33 +0530

    Dummy commit

--- hough.cpp
+++ hough.cpp
@@ -2,6 +2,8 @@
 #include <opencv2/imgproc/imgproc.hpp>
 
 #include <iostream>
+#include <string>
+
 
 using namespace cv;
 using namespace std;
@@ -18,44 +20,27 @@ int main( int argc, char *argv[] )
 	}
 
 	Mat dst, cdst;
-	Canny( src, dst, 50, 200, 3 );
-	cvtColor( dst, cdst, CV_GRAY2BGR );
-
-#if 0
-	vector<Vec2f> lines;
-	HoughLines( dst, lines, 1, CV_PI/180, 100, 0 0 );
-
-	for( size_t i = 0; i < lines.size(); i++ )
+	for( int iter = 0; iter < 5; iter++ )
 	{
-		float rho = lines[ i ][ 0 ], theta = lines[ i ][ 1 ];
-		Point pt1, pt2;
-		double a = cos( theta ), b = sin( theta );
-		double x0 = a*rho, y0 = b*rho;
-		pt1.x = cvRound( x0 + 1000 * ( -b ) );
-		pt1.y = cvRound( y0 + 1000 * ( a ) );
-		pt2.x = cvRound( x0 - 1000 * ( -b ) );
-		pt2.y = cvRound( y0 - 1000 * ( a ) );
-		line( cdst, pt1, pt2, Scalar( 0, 0, 255 ), 3, CV_AA );
+		Canny( src, dst, 50 + ( iter * 50 ), 750, 3 );
+		cvtColor( dst, cdst, CV_GRAY2BGR );
+
+		vector<Vec4i> lines;
+		HoughLinesP( dst, lines, 1, CV_PI/180, 50, 50, 10 );
+		for( size_t i = 0; i < lines.size(); i++ )
+		{
+			Vec4i l = lines[ i ];
+			line( cdst, Point( l[ 0 ], l[ 1 ] ), Point( l[ 2 ], l[ 3 ] ), Scalar( 0, 0, 255 ), 3, CV_AA );
+		}
+
+		vector<int> compression_params;
+	    compression_params.push_back(CV_IMWRITE_PNG_COMPRESSION);
+	    compression_params.push_back(9);
+
+	    stringstream s;
+	    s << iter;
+		imwrite( s.str() + string( ".png" ), cdst, compression_params );
 	}
-
-#else
-	vector<Vec4i> lines;
-	HoughLinesP( dst, lines, 1, CV_PI/180, 50, 50, 10 );
-	for( size_t i = 0;i  < lines.size(); i++ )
-	{
-		Vec4i l = lines[ i ];
-		line( cdst, Point( l[ 0 ], l[ 1 ] ), Point( l[ 2 ], l[ 3 ] ), Scalar( 0, 0, 255 ), 3, CV_AA );
-	}
-
-#endif
-
-	vector<int> compression_params;
-    compression_params.push_back(CV_IMWRITE_PNG_COMPRESSION);
-    compression_params.push_back(9);
-
-	imshow( "source", src );
-	imshow( "detected lines", cdst );
-	imwrite( "houghed.png", cdst, compression_params );
 
 	waitKey();
 
