0eed16737ca8bc3a0ad5c575bb0c66e2a01f7e19
Vinay Rao <sr.vinay@gmail.com>
2013-03-31 13:20:41 +0530

    Finished basic SVM training routine

--- src/helpers.py
+++ src/helpers.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import pickle
+import sys
 
 def norm( array, p = 2 ):
     return sum( [ i ** p for i in array ] ) ** 0.5
@@ -9,3 +10,14 @@ def normalize( array ):
     l2norm = norm( array )
     for i in xrange( len( array ) ):
         array[ i ] = array[ i ] / l2norm
+
+def saveObject( object, file ):
+    try:
+        f = open( file, 'w' )
+    except Exception as e:
+        print e
+        sys.exit( 1 )
+    
+    pickle.dump( object, f )
+    f.close()
+    print "Saved object to :", file
--- src/svm_train.py
+++ src/svm_train.py
@@ -3,12 +3,12 @@
 import sys
 import cv2
 from sklearn import svm
-from scipy.cluster import kmeans2, vq
+from scipy.cluster.vq import kmeans2, vq
 import numpy
 
 import helpers
 
-FEATURE_TYPES = 500
+FEATURE_TYPES = 50
 
 class Category:
     def __init__( self, label, features = [] ):
@@ -17,7 +17,7 @@ class Category:
         self.bagofwords = []
     
     def add_feature( self, feature ):
-        self.features.extend( feature )
+        self.features.append( feature )
     
     def calc_bagofwords( self, centroids ):
         for feature in self.features:
@@ -27,3 +27,59 @@ class Category:
                 bow[ label ] += 1
             helpers.normalize( bow )
             self.bagofwords.append( bow )
+
+
+def train_routine( argv ):
+    training_file = argv[ 1 ]
+    svm_file = argv[ 2 ]
+    centroid_file = argv[ 3 ]
+    
+    surf = cv2.SURF( 250 )
+    categories = {}
+    ids = {}
+    id = 1
+    features = []
+    
+    for line in open( training_file ):
+        try:
+            category, path = line.split( ';' )
+        except:
+            print "Error: File not in proper format. Ensure: <category/class name>; <path to image of said category>"
+            sys.exit( 0 )
+        path = path.strip()
+        
+        try:
+            img = cv2.imread( path )
+        except Exception as e:
+            print e
+
+        keypoints, descriptors = surf.detectAndCompute( img, None )
+        
+        if not categories.has_key( category ):
+            categories[ category ] = Category( category )
+            ids[ category ] = id
+            id += 1
+        categories[ category ].add_feature( descriptors )
+        features.extend( descriptors )
+    
+    np_features = numpy.array( features )
+    centroids, labels = kmeans2( np_features, FEATURE_TYPES )
+    
+    X, Y = [], []
+    for category in categories:
+        categories[ category ].calc_bagofwords( centroids )
+        for bow in categories[ category ].bagofwords:
+            X.append( bow )
+            Y.append( ids[ category ] )
+    
+    lin_clf = svm.LinearSVC()
+    lin_clf.fit( X, Y )
+    
+    helpers.saveObject( lin_clf, svm_file )
+    helpers.saveObject( centroids, centroid_file )
+
+if __name__ == "__main__":
+    if( len( sys.argv ) != 4 ):
+        print "Usage: $python svm_train.py training_file svm_file centroid_file"
+        sys.exit( 1 )
+    train_routine( sys.argv )
