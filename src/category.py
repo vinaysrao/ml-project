1b69a2d90238f92e43fbfcb15834f929fca87222
Vinay Rao <sr.vinay@gmail.com>
2013-04-04 14:44:54 +0530

    category.py added

---
+++ src/category.py
@@ -0,0 +1,33 @@
+import numpy
+from scipy.cluster.vq import vq
+
+import helpers
+from helpers import FEATURE_TYPES
+
+
+class Category:
+    """
+    This class represents one category that is encountered
+    while training. It is used to calculate the bag of words
+    vector, after all possible feature types are added to this
+    category.
+    """
+    def __init__(self, label=None, features=[]):
+        self.label = label
+        self.features = features
+        self.bagofwords = []
+
+    def add_feature(self, feature):
+        """Add one feature vector from one image"""
+        self.features.append(feature)
+
+    def calc_bagofwords(self, centroids):
+        """Calculate bag of words using the features
+        added to an object."""
+        for feature in self.features:
+            labels, _ = vq(numpy.array(feature), centroids)
+            bow = numpy.zeros(FEATURE_TYPES)
+            for label in labels:
+                bow[label] += 1
+            helpers.normalize(bow)
+            self.bagofwords.append(bow)
